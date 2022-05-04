7c435f63b1038f3b40a2f083fba2cdc384e25a4d
Emine Çığ <emine.cig67@gmail.com>
2022-05-04 10:53:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee={
+    name:"emine",
+    streetAdress:  "incilipinar",
+
+
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmp={...employee,[key]:value};
+    return newEmp;
+    
+    
+    
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+
+    const newObj={...employee};
+
+        delete newObj[key];
+        return newObj;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key] ;
+    return employee;
+}
